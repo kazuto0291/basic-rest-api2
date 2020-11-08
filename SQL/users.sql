61b5e17092a57ff0b6488081a5c9c30b9e838f08
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-08 18:42:20 +0900

    sql

--- SQL/users.sql
+++ SQL/users.sql
@@ -6,3 +6,9 @@ CREATE TABLE users (
   updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
   date_of_birth TEXT
 );
+
+INSERT INTO users (name, profile) VALUES ("Subaru","エミリアたんマジ天使！");
+INSERT INTO users (name, profile) VALUES ("Emilia", "もう、スバルのおたんこなす！");
+INSERT INTO users (name, profile) VALUES ("Ram", "いいえお客様、きっと生まれてきたのが間違いだわ");
+INSERT INTO users (name, profile) VALUES ("Rem", "はい、スバルくんのレムです。");
+INSERT INTO users (name, profile) VALUES ("Roswaal", "君は私にな-兄を望むのかな?");

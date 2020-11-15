f3f88ce4c5d4be6e0c1ba6486468ac7e8b258ab9
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-15 14:47:25 +0900

    re-update-user

--- app/app.js
+++ app/app.js
@@ -135,26 +135,53 @@ app.post('/api/v2/users', async (req, res) => {
   const profile =req.body.profile ? req.body.profile : ""
   const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
 
-  const run = async (sql) => {
-    return new Promise((resolve, reject) => {
-      db.run(sql, (err) => {
-        if(err) {
-          res.status(500).send(err)
-          return reject()
-        } else {
-          res.json({message: "新規ユーザーを作成しました"})
-          return resolve()
-        }
-      })
-    })
-  }
+  // const run = async (sql) => {
+  //   return new Promise((resolve, reject) => {
+  //     db.run(sql, (err) => {
+  //       if(err) {
+  //         res.status(500).send(err)
+  //         return reject()
+  //       } else {
+  //         res.json({message: "新規ユーザーを作成しました"})
+  //         return resolve()
+  //       }
+  //     })
+  //   })
+  // }
 
-  await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}","${profile}", "${dateOfBirth}")`)
+  await run(
+    `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}","${profile}", "${dateOfBirth}")`
+    ,db
+    ,res
+    ,"新規ユーザーを作成しました"
+    )
+  db.close()
+})
+
+// Update user data
+app.put(`/api/v2/users/:id`, async (req, res) => {
+  // Connect database
+
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  // 現在のユーザー情報を取得する
+  db.get(`SELECT * FROM users WHERE id=${id}`, async (err, row) => {
+    const name =req.body.name ? req.body.name : row.name
+    const profile = req.body.profile ? req.body.profile : row.profile
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+
+    await run(
+      `UPDATE users SET name="${name}",profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+      db,
+      res,
+      "ユーザー情報を更新しました"
+    )
+  })
   db.close()
 })
 
 
-// 
 
 
 const port = process.env.PORT || 3000;

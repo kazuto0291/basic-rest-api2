893bcdcb3f2a2547de221fc3f3c460c242deef3a
kazuto0291 <zutozuto03170291@gmail.com>
2020-11-15 15:48:00 +0900

    ユーザーフォームとcrateuser-apiをつなげる

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,9 +1,27 @@
 const indexModule = (() => {
-  // 検索ボタンをクリックしたときのイベントリスナー設定
-  document.getElementById('search-btn').addEventListener('click', ()=> {
-    console.log('クリックした！')
-     return searchModule.searchUsers()
-  })
-  // UsersモジュールのfetchAllUsersメソッドを呼び出す
-  return usersModule.fetchAllUsers()
+  const path = window.location.pathname
+
+  switch (path) {
+    case '/':
+      // 検索ボタンをクリックしたときのイベントリスナー設定
+      document.getElementById('search-btn').addEventListener('click', ()=> {
+        console.log('クリックした！')
+         return searchModule.searchUsers()
+      })
+      // UsersモジュールのfetchAllUsersメソッドを呼び出す
+      return usersModule.fetchAllUsers()
+
+    case '/create.html':
+      document.getElementById('save-btn').addEventListener('click', () => {
+        return usersModule.createUser()
+      })
+
+      document.getElementById('cancel-btn').addEventListener('click', () => {
+        return window.location.href = '/'
+      })
+      break;
+
+    default:
+      break;//デフォルトは何もしません。
+  }
 })()
